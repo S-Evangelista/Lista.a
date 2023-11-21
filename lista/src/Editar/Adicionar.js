4f01377793b7f3ff36a1603742b56c82149b599f
S-Evangelista <128431410+S-Evangelista@users.noreply.github.com>
2023-11-21 18:36:09 -0300

    Update Adicionar.js

--- lista/src/Editar/Adicionar.js
+++ lista/src/Editar/Adicionar.js
@@ -1,83 +1,74 @@
-import  { useState } from 'react';
-import {  StyleSheet } from 'react-native';
+import { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, FlatList } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 
-
 const Adicionar = ({ route }) => {
-    const { taskId, taskText, subitems, setSubitems } = route.params;
-    const [subitemText, setSubitemText] = useState('');
-    const [editingSubitemId, setEditingSubitemId] = useState(null);
-    const [editedSubitemText, setEditedSubitemText] = useState('');
-  
-    const addSubitem = () => {
-      if (subitemText.trim() !== '') {
-        const newSubitem = { id: subitems.length.toString(), text: subitemText };
-        setSubitems([...subitems, newSubitem]);
-        setSubitemText('');
-      }
-    };
-  
-    const removeSubitem = (subitemId) => {
-      const updatedSubitems = subitems.filter((item) => item.id !== subitemId);
-      setSubitems(updatedSubitems);
-    };
-  
-    const editSubitem = (subitemId, newText) => {
-      const updatedSubitems = subitems.map((item) =>
-        item.id === subitemId ? { ...item, text: newText } : item
-      );
-      setSubitems(updatedSubitems);
-      setEditingSubitemId(null);
-    };
-  
-    const startEditingSubitem = (subitemId, currentText) => {
-      setEditingSubitemId(subitemId);
-      setEditedSubitemText(currentText);
-    };
-  
-    const cancelEditingSubitem = () => {
-      setEditingSubitemId(null);
-      setEditedSubitemText('');
-    };
-}
+  const { taskId, taskText, subitems, setSubitems } = route.params;
+  const [subitemText, setSubitemText] = useState('');
+  const [editingSubitemId, setEditingSubitemId] = useState(null);
+  const [editedSubitemText, setEditedSubitemText] = useState('');
 
+  const addSubitem = () => {
+    if (subitemText.trim() !== '') {
+      const newSubitem = { id: subitems.length.toString(), text: subitemText };
+      setSubitems([...subitems, newSubitem]);
+      setSubitemText('');
+    }
+  };
 
-return (
-    <View >
-      <Text >{taskText}</Text>
+  const removeSubitem = (subitemId) => {
+    const updatedSubitems = subitems.filter((item) => item.id !== subitemId);
+    setSubitems(updatedSubitems);
+  };
+
+  const editSubitem = (subitemId, newText) => {
+    const updatedSubitems = subitems.map((item) =>
+      item.id === subitemId ? { ...item, text: newText } : item
+    );
+    setSubitems(updatedSubitems);
+    setEditingSubitemId(null);
+  };
+
+  const startEditingSubitem = (subitemId, currentText) => {
+    setEditingSubitemId(subitemId);
+    setEditedSubitemText(currentText);
+  };
+
+  const cancelEditingSubitem = () => {
+    setEditingSubitemId(null);
+    setEditedSubitemText('');
+  };
+
+  return (
+    <View>
+      <Text>{taskText}</Text>
       <FlatList
         data={subitems}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View >
+          <View>
             {editingSubitemId === item.id ? (
-              <View >
+              <View>
                 <TextInput
                   value={editedSubitemText}
                   onChangeText={(text) => setEditedSubitemText(text)}
                 />
-                <TouchableOpacity
-                  style={styles.editingButton}
-                  onPress={() => editSubitem(item.id, editedSubitemText)}
-                >
-                  <Text style={styles.buttonText}>Salvar</Text>
+                <TouchableOpacity onPress={() => editSubitem(item.id, editedSubitemText)}>
+                  <Text>Salvar</Text>
                 </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.editingButton}
-                  onPress={cancelEditingSubitem}
-                >
-                  <Text style={styles.buttonText}>Cancelar</Text>
+                <TouchableOpacity onPress={cancelEditingSubitem}>
+                  <Text>Cancelar</Text>
                 </TouchableOpacity>
               </View>
             ) : (
-              <View style={styles.subitemTextContainer}>
+              <View>
                 <Text>{item.text}</Text>
-                <View style={styles.subitemButtonContainer}>
+                <View>
                   <TouchableOpacity onPress={() => startEditingSubitem(item.id, item.text)}>
-                    <Text style={styles.editButton}>Editar</Text>
+                    <Text>Editar</Text>
                   </TouchableOpacity>
                   <TouchableOpacity onPress={() => removeSubitem(item.id)}>
-                    <Text style={styles.deleteButton}>Excluir</Text>
+                    <Text>Excluir</Text>
                   </TouchableOpacity>
                 </View>
               </View>
@@ -85,24 +76,20 @@ return (
           </View>
         )}
       />
-      <View style={styles.inputContainer}>
+      <View>
         <TextInput
-          style={styles.input}
           placeholder="Digite um subitem"
           value={subitemText}
           onChangeText={(text) => setSubitemText(text)}
         />
-        <TouchableOpacity style={styles.addButton} onPress={addSubitem}>
-          <Text style={styles.buttonText}>Adicionar Subitem</Text>
+        <TouchableOpacity onPress={addSubitem}>
+          <Text>Adicionar Subitem</Text>
         </TouchableOpacity>
       </View>
     </View>
   );
+};
 
-  
 const Stack = createStackNavigator();
 
-});
-
 export default Adicionar;
-
